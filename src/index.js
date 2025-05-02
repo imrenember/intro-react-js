82338bd5c0e4d5f6ac760141ff033168dceb5178
Imrenember <renembern@gmail.com>
2025-05-02 14:59:44 -0500

    beging destructure arrays

--- src/index.js
+++ src/index.js
@@ -1,30 +1,4 @@
-// //Destructuration or assigment destructu...
+const personajes = ['Goku', 'Vegeta', 'Trunks'];
+const [p1] = personajes;
 
-const person = {
-    name: 'Renember',
-    lastname: 'Nino',
-    age: 23,
-    password: 1234,
-  
-};
-
-
-// const {name, lastname, age} = person;
-
-
-// console.log(name, lastname)
-// console.log(lastname)
-
-const getContext = ({password,age}) => {
-    // const {name, lastname, age} = person;
-    //  console.log(name, lastname, age);
-    return{
-        namePassword: password,
-        ages: age,
-    }
-}
-
-
-const {namePassword, ages} = getContext(person);
-
-console.log(namePassword, ages);
+// console,log(p1);
